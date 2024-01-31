95a91903f91f83e227cbc2c5b778fd6a24f3fbd7
Yash-Ainapure <ainapureyash2@gmail.com>
2024-01-31 15:53:37 +0530

    initial login and firebase

---
+++ app/src/main/java/com/example/rich_stich/Home.java
@@ -0,0 +1,15 @@
+package com.example.rich_stich;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //hello yahs
+
+    }
+}
---
+++ app/src/main/java/com/example/rich_stich/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.rich_stich;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button Login;
+    EditText Username, Password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+
+        Button LoginButton = (Button) findViewById(R.id.buttonLogin);
+        EditText Username = (EditText) findViewById(R.id.editTextEmail);
+        EditText Password = (EditText) findViewById(R.id.editTextTextPassword);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Username.getText().toString();
+                String password = Password.getText().toString();
+
+                // Check if email and password are not empty
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    // Use Firebase Authentication to sign in
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(MainActivity.this, Home.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    // Display a message if email or password is empty
+                    Toast.makeText(getApplicationContext(), "Please enter email and password", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+    }
+}
